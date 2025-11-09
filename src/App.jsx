fec681f3f059508162a11cabb78ef07f18cebab3
DhuySoS <154112386+DhuySoS@users.noreply.github.com>
2025-11-10 00:58:43 +0700

    add layout and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Toaster, toast } from "sonner";
 import HomePage from "./pages/HomePage";
+import MasterLayout from "./components/layout/MasterLayout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return(
   <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<MasterLayout />} >
+          <Route index element={<HomePage />} />
+        </Route>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     </BrowserRouter>
   </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,13 @@
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
-import HeroSection from "@/components/layout/HeroSection";
+import HeroSection from "@/components/home/HeroSection";
 import React from "react";
 
 function HomePage() {
   return (
-    <div className="min-h-screen w-full bg-white relative overflow-hidden">
-      {/* Blue Corner Glow Background */}
-      <div
-        className="absolute inset-0 z-0"
-        style={{
-          backgroundImage: `
-        radial-gradient(circle 600px at 0% 200px, #bfdbfe, transparent),
-        radial-gradient(circle 600px at 100% 200px, #bfdbfe, transparent)
-      `,
-        }}
-      />
-      {/* Your Content Here */}
-      <div className="container mx-auto pt-8 relative z-10">
-        <div className="w-full space-y-10">
-          <Header />
-          <HeroSection />
-          <Footer />
-        </div>
-      </div>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 }
 
